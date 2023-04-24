94eb1dbe09e44030290a8f58b2a01242619af1fc
Yury Yurevich <git@y10h.com>
2023-04-24 00:11:36 -0700

    Add parsing for photo albums.
    I haven't checked the functionality though. Also cleaned up use of
    keys in parsed items and meta.

--- src/r2d2/scrapmetal/spiders/d2_spider.py
+++ src/r2d2/scrapmetal/spiders/d2_spider.py
@@ -5,6 +5,8 @@ import scrapy
 PATTERN_CAR = re.compile(r"/r/[a-z0-9_]+/[a-z0-9_]+/[\d]+/")
 PATTERN_CAR_LOGBOOK = re.compile(r"/r/[a-z0-9_]+/[a-z0-9_]+/[\d]+/logbook")
 PATTERN_PHOTO_ALBUM = re.compile("/s/a/[a-zA-Z0-9]+")
+PATTERN_PHOTO_POST = re.compile("/s/[a-zA-Z0-9]+")
+PATTERN_PHOTO_IMAGE = re.compile("https://a.d-cd.net/[a-zA-Z0-9_-]+.jpg")
 
 KIND_USER_PROFILE = "UserProfile"
 KIND_CAR = "Car"
@@ -14,14 +16,22 @@ KIND_PHOTO = "Photo"
 KIND_BLOG_POST = "BlogPost"
 KIND_PHOTO_POST = "PhotoPost"
 
+# If a page is a element in a container,
+# then origin is an URL of the container.
 META_KEY_ORIGIN = "scrapmetal_origin"
-META_KEY_KIND = "scrapmetal_kind"
+# Type of the entry.
+# Parent is a root for a few collections.
+# Generally, it's a car page.
 META_KEY_PARENT = "scrapmetal_parent"
 
+KEY_KIND = "kind"
 KEY_URL = "url"
 KEY_TITLE = "title"
 KEY_DESCRIPTION = "description"
 KEY_PUBLISHED = "published"
+KEY_ORIGIN = "origin"
+KEY_PARENT = "parent"
+KEY_PHOTO_URL = "photo_url"
 
 USER_PROFILE_TEMPLATE = "https://www.drive2.ru/users/{username}/"
 
@@ -40,10 +50,6 @@ class D2ExperimentalSpider(scrapy.Spider):
         yield scrapy.Request(url=url, callback=self.parse_user_profile)
 
     def parse_user_profile(self, response):
-        meta = {
-            META_KEY_ORIGIN: response.url,
-            META_KEY_KIND: KIND_USER_PROFILE,
-        }
         next_links = response.css("a.u-link-area::attr(href)").getall()
         for next_link in next_links:
             if next_link:
@@ -53,30 +59,27 @@ class D2ExperimentalSpider(scrapy.Spider):
                     callback = self.parse_photo_album
                 else:
                     self.log(
-                        f"Found a link {next_link} from user profile "
+                        f"Found a link {next_link} from the user profile "
                         "but don't know what to do with it"
                     )
                     continue
                 next_url = response.urljoin(next_link)
                 self.log(f"Found next link {next_link} -- callback is {callback}.")
-                yield scrapy.Request(next_url, callback=callback, meta=meta)
+                yield scrapy.Request(next_url, callback=callback)
 
     def parse_car(self, response):
         meta = {
-            META_KEY_ORIGIN: response.url,
-            META_KEY_KIND: KIND_CAR,
             META_KEY_PARENT: response.url,
         }
         car_title = response.css("h1.x-title::text").get().strip()
         car_description = response.css("div.c-car-desc__text").get()
         publish_date = (
             response.css("div.c-car-desc")
-            .xpath("./meta[@itemprop='datePublished']")
-            .css("::attr('content')")
+            .xpath("./meta[@itemprop='datePublished']/@content")
             .get()
         )
         yield {
-            META_KEY_KIND: KIND_CAR,
+            KEY_KIND: KIND_CAR,
             KEY_URL: response.url,
             KEY_TITLE: car_title,
             KEY_DESCRIPTION: car_description,
@@ -85,11 +88,12 @@ class D2ExperimentalSpider(scrapy.Spider):
         car_photos = response.css("a.c-lightbox-anchor::attr(href)").getall()
         next_links = response.css("h3 a.c-link::attr(href)").getall()
         for car_photo_url in car_photos:
-            # Do not schedule parsing photo since we don't download photos yet.
+            # We don't download photos yet.
             yield {
-                META_KEY_KIND: KIND_PHOTO,
+                KEY_KIND: KIND_PHOTO,
                 KEY_URL: car_photo_url,
-                META_KEY_PARENT: meta[META_KEY_PARENT],
+                KEY_PARENT: meta[META_KEY_PARENT],
+                KEY_ORIGIN: response.url,
             }
         for next_link in next_links:
             if next_link:
@@ -99,7 +103,7 @@ class D2ExperimentalSpider(scrapy.Spider):
                     callback = self.parse_photo_album
                 else:
                     self.log(
-                        f"Found a link {next_link} from car page "
+                        f"Found a link {next_link} from the car page "
                         f"{response.url} but don't know what to do with it"
                     )
                     continue
@@ -108,27 +112,77 @@ class D2ExperimentalSpider(scrapy.Spider):
                 yield scrapy.Request(next_url, callback=callback, meta=meta)
 
     def parse_photo_album(self, response):
-        {
-            META_KEY_ORIGIN: response.url,
-            META_KEY_KIND: KIND_PHOTO_ALBUM,
+        meta = {
+            META_KEY_ORIGIN: response.meta.get(META_KEY_ORIGIN) or response.url,
             META_KEY_PARENT: response.meta.get(META_KEY_PARENT),
         }
-        # Does not support parsing yet.
-        yield {
-            META_KEY_KIND: KIND_PHOTO_ALBUM,
-            META_KEY_PARENT: response.meta.get(META_KEY_PARENT),
-            KEY_URL: response.url,
-        }
+        title = response.css("h1.x-title::text").get()
+        # If the page origin is the same as current page, then it's the photo
+        # main photo album and not one of its pages, thus we should return the payload.
+        if meta[META_KEY_ORIGIN] == response.url:
+            yield {
+                KEY_KIND: KIND_PHOTO_ALBUM,
+                KEY_TITLE: title,
+                KEY_URL: response.url,
+                KEY_PARENT: meta.get(META_KEY_PARENT),
+            }
+        photo_posts = response.css("div.c-snaps-preview a::attr('href')").getall()
+        for photo_post in photo_posts:
+            if photo_post:
+                if PATTERN_PHOTO_POST.match(photo_post):
+                    yield scrapy.Request(
+                        photo_post, callback=self.parse_photo_post, meta=meta
+                    )
+        next_page = response.xpath(
+            "//a[has-class('c-pager__link')][@rel='next']/@href"
+        ).get()
+        if next_page:
+            next_url = response.urljoin(next_page)
+            yield scrapy.Request(next_url, callback=self.parse_photo_album, meta=meta)
+
+    def parse_photo_post(self, response):
+        username = (
+            response.css("a.c-username").xpath("./span[@itemprop='name']/text()").get()
+        )
+        photo_description = response.xpath("//div[@itemprop='description']/*").get()
+        photo_url = response.css("a.c-lightbox-anchor::attr('href')").get() or ""
+        publish_date = response.xpath(
+            "//meta[@property='article:published_time']/@content"
+        ).get()
+        match = PATTERN_PHOTO_IMAGE.match(photo_url)
+        if not photo_url:
+            self.log(f"Photo page {response.url} doesn't have a link to the photo.")
+        elif photo_url and not match:
+            self.log(
+                f"Photo url {photo_url} from {response.url} is not recognized "
+                "as an url to a photo"
+            )
+        elif username != getattr(self, "username"):
+            self.log(
+                "It looks like spider accidentally crawls a photo which "
+                f"isn't owned by a user: photo post {response.url} its user {username}."
+            )
+        else:
+            yield {
+                KEY_KIND: KIND_PHOTO_POST,
+                KEY_DESCRIPTION: photo_description,
+                KEY_PUBLISHED: publish_date,
+                KEY_PARENT: response.meta.get(META_KEY_PARENT),
+                KEY_ORIGIN: response.meta.get(META_KEY_ORIGIN),
+            }
+            # We don't download photos yet.
+            yield {
+                KEY_KIND: KIND_PHOTO,
+                KEY_URL: photo_url,
+                KEY_PARENT: response.meta.get(META_KEY_PARENT),
+                KEY_ORIGIN: response.url,
+            }
 
     def parse_logbook(self, response):
         # Does not support parsing yet.
-        {
-            META_KEY_ORIGIN: response.url,
-            META_KEY_KIND: KIND_CAR_LOGBOOK,
-            META_KEY_PARENT: response.meta.get(META_KEY_PARENT),
-        }
+
         yield {
-            META_KEY_KIND: KIND_CAR_LOGBOOK,
-            META_KEY_PARENT: response.meta.get(META_KEY_PARENT),
+            KEY_KIND: KIND_CAR_LOGBOOK,
+            KEY_PARENT: response.meta.get(META_KEY_PARENT),
             KEY_URL: response.url,
         }
